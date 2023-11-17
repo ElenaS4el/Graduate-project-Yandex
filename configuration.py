4ddf9b57a8357ab5f49fc898ed9d1654de9c7ca7
ElenaS4el <148678905+ElenaS4el@users.noreply.github.com>
2023-11-17 22:49:41 +0300

    files

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://5ec2ab58-c9e8-440c-8ed0-4b29765e9e3e.serverhub.praktikum-services.ru" # адрес сервера
+CREATE_COURIER = "/api/v1/courier"   # создание курьера
+CREATE_ORDER = "/api/v1/orders"  # создание заказа
+ORDER_INFORMATION = "/api/v1/orders/track?t="   # получить информацию о заказе по его номеру
